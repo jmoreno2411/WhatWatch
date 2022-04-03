476b887711d7e9b5d42ec7f84fc2fd878d32ca74
jmoreno2411 <97634678+jmoreno2411@users.noreply.github.com>
2022-04-03 11:52:51 +0200

    commit with movies

---
+++ app/src/main/java/es/upv/dadm/whatwatch/Movie.java
@@ -0,0 +1,79 @@
+package es.upv.dadm.whatwatch;
+
+import java.util.Date;
+
+public class Movie {
+    private Integer id;
+    private String title;
+    private String overview; // Sinopsis
+    private Float vote_average; // Nota
+    private Date release_date;
+    private Integer vote_count; // Votos recibidos
+    //private String poster_path; // Imagen de portada
+
+    public Movie(Integer id, String title, String overview, Float vote_average, Date release_date, Integer vote_count) {
+        this.id = id;
+        this.title = title;
+        this.overview = overview;
+        this.vote_average = vote_average;
+        this.release_date = release_date;
+        this.vote_count = vote_count;
+        //this.poster_path = poster_path;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public Float getVote_average() {
+        return vote_average;
+    }
+
+    public void setVote_average(Float vote_average) {
+        this.vote_average = vote_average;
+    }
+
+    public Date getRelease_date() {
+        return release_date;
+    }
+
+    public void setRelease_date(Date release_date) {
+        this.release_date = release_date;
+    }
+
+    public Integer getVote_count() {
+        return vote_count;
+    }
+
+    public void setVote_count(Integer vote_count) {
+        this.vote_count = vote_count;
+    }
+    /*
+    public String getPoster_path() {
+        return poster_path;
+    }
+    public void setPoster_path(String poster_path) {
+        this.poster_path = poster_path;
+    }
+     */
+}
